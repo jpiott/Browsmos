9548dccbe31152a0eb52ad51c99246a59c37a7a4
Jonathan Piott <jpiott@unt.edu>
2014-06-03 20:14:12 -0500

    Removed javascript from index page and added converted code to browsmos.dart.

--- browsmos.dart
+++ browsmos.dart
@@ -1,3 +1,29 @@
+import 'dart:html';
+import 'include/World.dart';
+
+// Engine globals
+int fps = 30;
+int mspf = 1000 ~/ fps;
+int updateInterval;
+List center;
+int viewport_radius;
+
+// Game globals
+World world;
+
 void main() {
+  
+  CanvasElement canvas = querySelector('#canvas');
+  CanvasRenderingContext2D ctx = canvas.getContext('2d');
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  center = [canvas.width ~/ 2, canvas.height ~/ 2];
+  
+  // Initialize the world
+  world = new World(canvas);
+  world.load_level();
+  
+  // Animate!
+  window.animationFrame.then((value) => world.update());
 
 }
